c175aa72b852a5617aad5f57193245188fd7c6fe
Unzor <71789103+Unzor@users.noreply.github.com>
2021-07-13 10:07:21 -0500

    Create SiteMaker.lua

---
+++ rblx_code/SiteMaker.lua
@@ -0,0 +1,47 @@
+--[[
+SiteCreator v1.0
+
+A module that lets you create a website and publish it.
+
+(Can only be used in normal scripts because this uses HTTP service)
+
+-----------------------------------------------SETUP---------------------------------------------------------
+1. Turn on HTTP services.
+2. Add this script to ReplicatedStorage.
+3. Create a new script and require() this script.
+
+-----------------------------------------------USAGE---------------------------------------------------------
+
+local siteCreator=require(game.ReplicatedStorage.SiteCreator)
+local created_site=siteCreator.makeSite(HTML CODE HERE)
+print(created_site) --Print URL of created website
+]]--
+
+local h={}
+
+h.makeSite=function(code)
+	local val=string.gsub(code, [[
+
+]], "(NEWLINE)")
+
+	val=string.gsub(val, [["]], [[(DOUBLEQUOTE)]])
+
+	val=string.gsub(val, [[{]], "")
+
+	val=string.gsub(val, [[}]], "")
+	
+	val=string.gsub(val, [[ ]], "(SPACE)")
+
+	local array=[[{
+"html": "]]..val..[["
+}]]
+	
+	print(array)
+	
+	local site=game.HttpService:PostAsync("https://makeasite.seven7four4.repl.co/newSite", array)
+	local siteData=game.HttpService:JSONDecode(site)
+
+return siteData.site
+end
+
+return h
